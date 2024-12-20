a3c6b463852073928abf566dde3c58c1825b6abd
ShawnFrost26 <sb262202@gmail.com>
2024-12-20 17:24:37 +0530

    fix css

--- src/components/VideoTitle.jsx
+++ src/components/VideoTitle.jsx
@@ -4,7 +4,7 @@ const VideoTitle = ({ title, overview }) => {
       <h1 className="text-6xl font-bold">{title}</h1>
       <p className="py-6 text-lg w-1/3">{overview}</p>
       <div className="">
-        <button className="bg-white text-black p-4 px-16 text-xl font-bold rounded-md bg-white hover:bg-opacity-80">
+        <button className="text-black p-4 px-16 text-xl font-bold rounded-md bg-white hover:bg-opacity-80">
           ▶️ Play
         </button>
         <button className="bg-gray-500 text-white p-4 px-12 text-lg mx-2 bg-opacity-50 rounded-md hover:bg-opacity-70">
