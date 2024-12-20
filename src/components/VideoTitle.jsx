866ea7d209b5749c5c81098ce14e0b787cc1ff24
ShawnFrost26 <sb262202@gmail.com>
2024-12-20 17:04:33 +0530

    fixed css

--- src/components/VideoTitle.jsx
+++ src/components/VideoTitle.jsx
@@ -1,14 +1,14 @@
 const VideoTitle = ({ title, overview }) => {
   return (
-    <div className="pt-36 px-12">
+    <div className="pt-[20%] px-24 absolute text-white bg-gradient-to-r from-black w-full aspect-video">
       <h1 className="text-6xl font-bold">{title}</h1>
       <p className="py-6 text-lg w-1/3">{overview}</p>
       <div className="">
-        <button className="bg-black text-white p-4 px-16 text-xl rounded-md">
-          ▶️Play
+        <button className="bg-white text-black p-4 px-16 text-xl font-bold rounded-md bg-white hover:bg-opacity-80">
+          ▶️ Play
         </button>
-        <button className="bg-gray-500 text-black p-4 px-12 text-lg mx-2 bg-opacity-50 rounded-md">
-          More Info
+        <button className="bg-gray-500 text-white p-4 px-12 text-lg mx-2 bg-opacity-50 rounded-md hover:bg-opacity-70">
+         ℹ️ More Info
         </button>
       </div>
     </div>
