cd9c9983dda6f0e8c33e38581674b1b0bfd36eca
ShawnFrost26 <sb262202@gmail.com>
2024-12-21 11:27:57 +0530

    added movie list

---
+++ src/components/MovieList.jsx
@@ -0,0 +1,26 @@
+import MovieCard from "./MovieCard";
+
+const MovieList = ({ title, movies }) => {
+  //   console.log("MovieCard", movies);
+  if (!movies) return;
+
+  return (
+    <div className="px-6">
+      <h1 className="text-3xl py-4 text-white">{title}</h1>
+      <div className="flex overflow-x-scroll">
+        <div></div>
+        <div className="flex">
+          {movies.map((movie) => (
+            <MovieCard
+              key={movie.id}
+              posterPath={movie.poster_path}
+              movieName={movie.title}
+            />
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default MovieList;
