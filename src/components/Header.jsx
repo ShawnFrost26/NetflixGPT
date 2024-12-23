623af208bab7974550a31d3d5511a1ed46fae24c
ShawnFrost26 <sb262202@gmail.com>
2024-12-23 17:19:21 +0530

    added gpt searchbar and multi language feature

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,12 +4,15 @@ import { useNavigate } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import { useEffect } from "react";
 import { addUser, removeUser } from "../utils/userSlice";
-import { LOGO } from "../utils/constants";
+import { LOGO, SUPPORTED_LANGUAGES } from "../utils/constants";
+import { toggleGptSearchView } from "../utils/gptSlice";
+import { changeLanguage } from "../utils/configSlice";
 
 const Header = () => {
   const dispatch = useDispatch();
   const navigate = useNavigate();
   const user = useSelector((store) => store.user);
+  const showGptSearch = useSelector((store) => store.gpt.showGptSearch);
 
   const handleSignOut = () => {
     signOut(auth)
@@ -44,15 +47,37 @@ const Header = () => {
     return () => unsubscribe();
   }, []);
 
+  const handleGptSearchClick = () => {
+    dispatch(toggleGptSearchView());
+  };
+
+  const handleLanguageChange = (e) => {
+    dispatch(changeLanguage(e.target.value));
+  };
+
   return (
     <div className="absolute px-8 py-2 bg-gradient-to-b from-black z-10 w-full flex justify-between">
-      <img
-        className="h-12"
-        src={LOGO}
-        alt="netflix-logo"
-      />
+      <img className="h-12" src={LOGO} alt="netflix-logo" />
       {user && (
         <div className="flex items-center gap-2">
+          {showGptSearch && (
+            <select
+              className="p-2 m-2 bg-gray-700 text-white rounded-lg"
+              onChange={handleLanguageChange}
+            >
+              {SUPPORTED_LANGUAGES.map((lang) => (
+                <option key={lang.identifier} value={lang.identifier}>
+                  {lang.name}
+                </option>
+              ))}
+            </select>
+          )}
+          <button
+            className="py-2 px-4 m-2 bg-purple-700 text-white rounded-lg"
+            onClick={handleGptSearchClick}
+          >
+            {showGptSearch ? "Homepage" : "GPT Search"}
+          </button>
           <img className="w-8 h-8" src={user?.photoURL} alt="user-icon" />
           <button
             onClick={handleSignOut}
--- src/utils/appStore.js
+++ src/utils/appStore.js
@@ -1,15 +1,16 @@
+import { configureStore } from "@reduxjs/toolkit";
+import userReducer from "./userSlice";
+import moviesReducer from "./moviesSlice";
+import gptReducer from "./gptSlice";
+import configReducer from "./configSlice";
 
-import {configureStore} from "@reduxjs/toolkit"
-import userReducer from "./userSlice"
-import moviesReducer from "./moviesSlice"
-
-const appStore = configureStore(
-    {
-        reducer: {
-            user: userReducer,
-            movies: moviesReducer,
-        }
-    }
-)
+const appStore = configureStore({
+  reducer: {
+    user: userReducer,
+    movies: moviesReducer,
+    gpt: gptReducer,
+    config: configReducer,
+  },
+});
 
 export default appStore;
