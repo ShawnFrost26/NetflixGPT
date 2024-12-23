5ee8a7929adf70534986ebb2039cedc33b0b29e2
ShawnFrost26 <sb262202@gmail.com>
2024-12-23 17:17:50 +0530

    added constants

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,13 +1,34 @@
-export const LOGO = "https://cdn.cookielaw.org/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png"
+export const LOGO =
+  "https://cdn.cookielaw.org/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png";
 
-export const user_avatar = "https://cdn-icons-png.flaticon.com/128/1144/1144709.png"
+export const user_avatar =
+  "https://cdn-icons-png.flaticon.com/128/1144/1144709.png";
 
 export const API_OPTIONS = {
-    method: 'GET',
-    headers: {
-      accept: 'application/json',
-      Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI0NzY4Y2VkODA2ODkzMzg3NGIyNjg0OTg2Yjg2OWExZSIsIm5iZiI6MTczNDYwMDEzMC4zMzc5OTk4LCJzdWIiOiI2NzYzZTVjMjA2OTdiNmM2YzFhYWZjMDYiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.kpsF8mZc24xypJNrocLXaqlfFlYva98Ya3_GnFLJdvw'
-    }
-  };
+  method: "GET",
+  headers: {
+    accept: "application/json",
+    Authorization:
+      "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI0NzY4Y2VkODA2ODkzMzg3NGIyNjg0OTg2Yjg2OWExZSIsIm5iZiI6MTczNDYwMDEzMC4zMzc5OTk4LCJzdWIiOiI2NzYzZTVjMjA2OTdiNmM2YzFhYWZjMDYiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.kpsF8mZc24xypJNrocLXaqlfFlYva98Ya3_GnFLJdvw",
+  },
+};
 
-  export const IMG_CDN_URL = "https://image.tmdb.org/t/p/w500/"
+export const IMG_CDN_URL = "https://image.tmdb.org/t/p/w500/";
+
+export const BG_URL =
+  "https://assets.nflxext.com/ffe/siteui/vlv3/729ce5c2-d831-436a-8c9d-f38fea0b99b3/web/IN-en-20241209-TRIFECTA-perspective_4aef76eb-7d5b-4be0-93c0-5f67320fd878_large.jpg";
+
+export const SUPPORTED_LANGUAGES = [
+  {
+    identifier: "en",
+    name: "English",
+  },
+  {
+    identifier: "hindi",
+    name: "Hindi",
+  },
+  {
+    identifier: "spanish",
+    name: "Spanish",
+  },
+];
