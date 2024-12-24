e678adca97de0c3d2568bdc051c40bb11cf9ad47
ShawnFrost26 <sb262202@gmail.com>
2024-12-24 12:39:47 +0530

    fetched gpt search results

--- src/components/GptSearchBar.jsx
+++ src/components/GptSearchBar.jsx
@@ -1,19 +1,77 @@
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import lang from "../utils/languageConstants";
+import { useRef } from "react";
+import openai from "../utils/openai";
+import model from "../utils/gemini";
+import { API_OPTIONS } from "../utils/constants";
+import { addGptMovieResult } from "../utils/gptSlice";
 
 const GptSearchBar = () => {
+  const dispatch = useDispatch();
+  const langKey = useSelector((store) => store.config.lang);
+  const searchText = useRef(null);
 
-const langKey = useSelector(store => store.config.lang)
+  //search movie in tmdb
+  const searchMovieTMDB = async (movie) => {
+    const data = await fetch(
+      "https://api.themoviedb.org/3/search/movie?query=" +
+        movie +
+        "&include_adult=false&language=en-US&page=1",
+      API_OPTIONS
+    );
+    const json = await data.json();
+    return json.results;
+  };
+
+  const handleGptSearchClick = async () => {
+    // console.log(searchText.current.value);
+    //make an api call to gpt api and get movie results
+
+    const gptQuery =
+      "Act as a Movie recommendation System and suggest some movies for the query: " +
+      searchText.current.value +
+      ". Only give me names of 5 movies, comma separated like the exampple result given ahead. Example result: Gadar, Sholay, Koi Mil Gaya, Spiderman, Singham";
+
+    //*********using openai key***********
+    // const gptResults = await openai.chat.completions.create({
+    //   messages: [{ role: "user", content: gptQuery }],
+    //   model: "gpt-3.5-turbo",
+    // });
+    // console.log("gptResults:::", gptResults.choices);
+
+    //***********using gemini api key**********
+    const result = await model.generateContent(gptQuery);
+
+    if (!result.response.text()) {
+    }
+    const gptMovies = result.response.text().split(",");
+
+    const promiseArray = gptMovies.map((movie) => searchMovieTMDB(movie));
+
+    const tmdbResults = await Promise.all(promiseArray);
+    console.log("tmdbResults:", tmdbResults);
+
+    dispatch(
+      addGptMovieResult({ movieNames: gptMovies, movieResults: tmdbResults })
+    );
+  };
 
   return (
     <div className="pt-[10%] flex justify-center">
-      <form className=" bg-black w-1/2 grid grid-cols-12">
+      <form
+        className=" bg-black w-1/2 grid grid-cols-12"
+        onSubmit={(e) => e.preventDefault()}
+      >
         <input
+          ref={searchText}
           type="text"
           className="p-4 m-4 col-span-9"
           placeholder={lang[langKey].gptSearchPlaceholder}
         />
-        <button className="py-2 px-4 m-4 bg-red-700 text-white rounded-lg col-span-3">
+        <button
+          className="py-2 px-4 m-4 bg-red-700 text-white rounded-lg col-span-3"
+          onClick={handleGptSearchClick}
+        >
           {lang[langKey].search}
         </button>
       </form>
